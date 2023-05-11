b57c4c89b5dfefcf9fbf57462db8808a69ebdab0
AnaelleIghil <anaelle.ighil@gmail.com>
2023-05-11 19:45:17 +0200

    cleaning

--- V2/script/local/deleteScripts/deleteDatabase.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Welcome to the delete database script"
-
-sudo -u posqtgres psql -f sql/deleteDatabase.sql
-
-echo "Database and Role deleted"
--- V2/script/local/deleteScripts/deleteTables.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Welcome to the supression table script "
-
-sudo -u postgres psql -f sql/deleteTables.sql
-
-echo "Tables deleted"
