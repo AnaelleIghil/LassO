42665367ff0a1035f6af6c8c1a100dd64106d97b
AnaelleIghil <anaelle.ighil@gmail.com>
2023-05-11 19:43:26 +0200

    big script on, still need to patch some issue with sql specifications

--- V2/initialScript2.sh
+++ V2/initialScript2.sh
@@ -1,22 +1,11 @@
-#!/bin/bash
-
-#    ___       _ _   _       _   ____            _       _   
-#   |_ _|_ __ (_) |_(_) __ _| | / ___|  ___ _ __(_)_ __ | |_ 
-#    | || '_ \| | __| |/ _` | | \___ \ / __| '__| | '_ \| __|
-#    | || | | | | |_| | (_| | |  ___) | (__| |  | | |_) | |_ 
-#   |___|_| |_|_|\__|_|\__,_|_| |____/ \___|_|  |_| .__/ \__|
-#                                                 |_|        
-
-
 PS3='Please, choose an option: '
 while true; do 
     clear
-    options=("Initial" "Delete" "Revert " "Quit")
+    options=("Initial" "Delete" "Revert" "Quit")
     select opt in "${options[@]}"
         do
             case $opt in 
                 "Initial")
-                    PS3='Choose where you want to initiate: '
                         options=("Initial Locally" "Initial Online" "Quit")
                         select opt in "${options[@]}"
                             do 
@@ -36,122 +25,97 @@ while true; do
                                 echo ".env variables implemented" 
                                 psql -U  "admin-LassO" -d "LassO" -f sql/tables.sql
                                 echo "creation of tables"
-                                break 3
+                                break 
                                 ;;
 
                             "Initial Online")
                                 echo "Not available yet"
-                                break 3
+                                sleep 1
+                                break 
                             ;;
                             "Quit")
-                            break 2
+                            break 
                             ;;
                             *) echo "invalid option $REPLY";;
                         esac
-                    done
-                    break
-                    ;;
-                
-                "Delete")
-                    PS3='Choose where you want to delete:  '
-                        while true; do
-                        options=("Locally" "Online" "Quit")
+                    done 
+                    break 1
+                ;;
+
+                "Delete") 
+                        options=("Delete Locally" "Delete Online" "Quit")
                         select opt in "${options[@]}"
                             do 
                                 case $opt in 
-                                "Locally")
-                                    PS3='What do you want to delete locally ? '
-                                         options=("Delete All" "Delete Tables " "Delete Database" "Quit")
-                                         select opt in "${options[@]}"
-                                            do
-                                                case $opt in
-                                                "Delete All")
-                                                        echo "Welcome to the full suppression script"
-                                                        sudo -u postgres psql -f sql/deleteTables.sql
-                                                        sleep 2
-                                                        echo "Tables deleted"
-                                                        sudo -u posqtgres psql -f sql/deleteDatabase.sql
-                                                        sleep 2
-                                                        echo "Database and Role deleted"
-                                                    break 3
-                                                    ;;
-                                                "Delete Tables")
-                                                    echo "Welcome to the supression table script "
-                                                    sleep 1
-                                                    sudo -u postgres psql -f sql/deleteTables.sql
-                                                    echo "Tables deleted"
-                                                    sleep 1
-                                                break 3
+                                "Delete Locally")
+                                    
+                                        options=("Delete All" "Delete Tables" "Delete Database" "Quit")
+                                        select opt in "${options[@]}"
+                                        do
+                                            case $opt in 
+                                            "Delete All")
+                                                echo "You choose to delete all, please wait"
+                                                sleep 1
+                                                sudo -u postgres psql -f sql/deleteTables.sql
+                                                echo "Tables deleted"
+                                                sleep 1
+                                                sudo -u postgres psql -f sql/deleteDatabase.sql
+                                                echo "Database and Role deleted"
+                                                sleep 1
+                                        break
+                                            ;;
+                                            "Delete Tables")
+                                                echo "You choose to delete tables " 
+                                        sleep 1
+                                        sudo -u postgres psql -f sql/deleteTables.sql
+                                        echo "Tables deleted"
+                                        sleep 1
+                                        break 
                                                 ;;
-                                                "Delete Database")
-                                                    echo "Welcome to the delete database script"
-                                                    sleep 1
-                                                    sudo -u posqtgres psql -f sql/deleteDatabase.sql
-                                                    echo "Database and Role deleted"
-                                                    sleep 1
-                                                break 3
+
+                                            "Delete Database")
+                                                echo "You choose to delete Database"
+                                        sleep 1
+                                        sudo -u posqtgres psql -f sql/deleteDatabase.sql
+                                        echo "Database and Role deleted"
+                                        sleep 1
+                                        break 
                                                 ;;
+
                                                 "Quit")
-                                                break 2
+                                                break
                                                 ;;
-                            "Online")
-                            PS3='What do you want to delete online ? '
-                                         options=("Delete All" "Delete Tables " "Delete Database" "Quit")
-                                         select opt in "${options[@]}"
-                                            do
-                                                case $opt in
-                                                "Delete All")
-                                                        echo "Welcome to the full suppression script"
-                                                        sudo -u postgres psql -f sql/deleteTables.sql
-                                                        echo "Tables deleted"
-                                                        sleep 2
-                                                        sudo -u posqtgres psql -f sql/deleteDatabase.sql
-                                                        echo "Database and Role deleted"
-                                                       sleep 2
-                                                    break 3
-                                                    ;;
-                                                "Delete Tables")
-                                                    echo "Welcome to the supression table script "
-                                                    sleep 2
-                                                    sudo -u postgres psql -f sql/deleteTables.sql
-                                                    echo "Tables deleted"
-                                                    sleep 2
-                                                break 3
-                                                ;;
-                                                "Delete Database")
-                                                    echo "Welcome to the delete database script"
-                                                   sleep 2
-                                                    sudo -u posqtgres psql -f sql/deleteDatabase.sql
-                                                    echo "Database and Role deleted"
-                                                    sleep 2
-                                                break 3
-                                                 ;;
-                                                "Quit")
-                                                break 2
-                                                ;;
-                                              
+                                                *) echo "invalid option $REPLY";;
+                                            esac
+                                        done
+                                break 2
+                                ;;
 
-                                            *) echo "invalid option $REPLY";;
-                                              
-                                            "Quit")
-                                                break 2
-                                                ;;
+                            "Delete Online")
+                                echo "Not available yet"
+                                sleep 1
+                                break 
+                            ;;
+                            "Quit")
+                            break 
+                            ;;
+                            *) echo "invalid option $REPLY";;
+                        esac
+                    done 
+                    break 1
+               ;;
 
-                                                esac
-                                                done
-                                                break 
-                                                ;;
-
-        
                 "Revert")
-                    echo "Not available yet"
-                    break 
-                    ;;
+                echo "revert loop on"
+                sleep 1
+                break 
+                ;;
 
-        "Quit")
-        break 3
-        ;;
-        *) echo "invalid option $REPLY";;
+                "Quit")
+    break 2
+;;
+
+         *) echo "invalid option $REPLY";;
         esac
     done
 done
