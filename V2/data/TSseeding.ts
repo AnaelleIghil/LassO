3af4b4820a994b4d802f8ce60b1b0d797d4eb60a
Anaëlle Ighil-Cardoso <anaelle.ighil@gmail.com>
2023-06-07 12:36:38 +0200

    first draft of generation method of data

--- V2/data/TSseeding.ts
+++ V2/data/TSseeding.ts
@@ -11,7 +11,7 @@ import { faker } from '@faker-js/faker/locale/fr';
 enum Size { XS='XS', S='S', M='M', L='L',XL='XL', XXL='XXL', XXXl='XXXL'};
 enum Gender { Male='M', female='F' };
 enum Status {Admin='Admin', Treasury='Treasury', Event='Event', Logistics='Logistics', Secretary='Secretary', Instruments='Instruments', Suits='Suits', Communications='Communications'};
-enum Instrument {Surdo='Surdo', Repinique='Repinique', Caixa='Caixa', Tamborim='Tamborim', Chocalho='Chocalho', Agogô='Agogô', Cuica='Cuica', Apito='Apito'};
+enum InstrumentType {Surdo='Surdo', Repinique='Repinique', Caixa='Caixa', Tamborim='Tamborim', Chocalho='Chocalho', Agogô='Agogô', Cuica='Cuica', Apito='Apito'};
 enum Group { Carnival='Carnival' , Mascarade='Mascarade', TragOul='TragOul', Rathma='Rathma', Bones='Bones', Heritage='Heritage', Jesseth='Jesseth', Pestilence='Pestilence', Inarius='Inarius'}
 /*
 const randomMember ={
@@ -76,13 +76,9 @@ const randomMessage ={
     subject : faker.word.adjective(),
     content: faker.lorem.paragraph({min:1, max:5}),
 } 
-*/
-const randomIssue = {
-    id_Member: faker.number.int({min: 1, max: 69}),
-    id_organization : faker.helpers.enumValue(Status),
-    content : faker.lorem.lines({min: 1, max : 5}),
-}
-console.log("randomIssue : ", randomIssue)
+
+/*
+
 /**
  * List of all interface used in theis code 
  * 
@@ -110,7 +106,7 @@ interface Suit {
     observation: string;
     size : string;
 }
-interface instrument {
+interface Instrument {
     name : string;
     type : string;
     observation : string;
@@ -119,8 +115,8 @@ interface instrument {
     sticker: boolean ; 
 }
 interface Treasury{
-    created_at : number;
-    paid_at : number;
+    created_at : Date;
+    paid_at : Date;
     subject : string;
     pole : string;
 }
@@ -136,7 +132,7 @@ interface Issue {
 interface Message {
     id_Member: number;
     id_organization : string;
-    send_at: number;
+    send_at: Date;
     subject :string;
     content: string;
 }
@@ -165,7 +161,7 @@ function generateMember(Nb_Users:number): any {
             size_up : faker.helpers.enumValue(Size) ,
             size_bottom : faker.helpers.enumValue(Size),
             status : faker.helpers.enumValue(Status),
-            instrument : faker.helpers.enumValue(Instrument),
+            instrument : faker.helpers.enumValue(InstrumentType),
             }
             Members.push(randomMember);
     }
@@ -174,4 +170,85 @@ function generateMember(Nb_Users:number): any {
 
 }
 
-generateMember(Nb_Users);
+//generateMember(Nb_Users);
+
+function generateSuit(Nb_Users:number): any{
+    const Suits: Array<Suit>=[];
+    for (let i=0; i< Nb_Users ; i+=1){
+        const randomSuit : Suit={
+            group : faker.helpers.enumValue(Group),
+            gender: faker.helpers.enumValue(Gender),
+            observation: faker.company.catchPhrase(),
+            size : faker.helpers.enumValue(Size),
+        }
+        Suits.push(randomSuit);
+    }
+    return Suits;
+}
+
+function generateInstrument(Nb_Users:number): any{
+    const Instruments: Array<Instrument>=[];
+    for (let i=0; i< Nb_Users; i+=1){
+        const randomInstrument : Instrument = {
+            name : faker.string.alpha(4),
+            type : faker.helpers.enumValue( InstrumentType),
+            observation : faker.company.catchPhrase(),
+            rods : faker.number.int(8),
+            weight : faker.number.float({min: 0.5, max: 50, precision: 0.1}),
+            sticker: faker.datatype.boolean()
+        }
+        Instruments.push(randomInstrument)
+    }
+    return Instruments;
+}
+
+function generateTreasury(Nb_Users:number): any{
+    const Treasuries: Array<Treasury>=[];
+    for (let i=0; i <Nb_Users; i+=1){
+        const randomTreasury: Treasury= {
+            created_at : faker.date.between({from: '20/04/2017', to: '15/05/2023' }),
+            paid_at : faker.date.between({from : '15/05/2023', to : '31/08/2033'}), 
+            subject : faker.company.catchPhraseDescriptor(),
+            pole : faker.helpers.enumValue(Status),
+        }
+        Treasuries.push(randomTreasury)
+    }
+    return Treasuries;
+}
+
+function generateTreasuryMember(Nb_Users:number):any{
+    const TreasuriesMember:Array<TreasuryMember>=[];
+    for (let i=0; i< Nb_Users; i+=1){
+        const randomTreasuryMember: TreasuryMember = {
+            status_deposit: faker.datatype.boolean(),
+            status_cotisation: faker.datatype.boolean(),
+        }
+        TreasuriesMember.push(randomTreasuryMember)
+    }
+    return TreasuriesMember;
+}
+
+function generateIssue(Nb_Users:number):any{
+    const Issues:Array<Issue>=[];
+    for (let i=0; i< Nb_Users; i+=1){
+        const randomIssue: Issue = {
+            id_Member: faker.number.int({min: 1, max: 69}),
+            id_organization : faker.helpers.enumValue(Status),
+            content : faker.lorem.lines({min: 1, max : 5}),
+        }
+        Issues.push(randomIssue)
+    }
+    return Issues;
+}
+function generateMessage(Nb_Users:number):any{
+    const Messages:Array<Message>=[];
+    for (let i=0; i< Nb_Users; i+=1){
+        const randomMessage : Message ={
+            id_Member: faker.number.int({min: 1, max: 69}),
+            id_organization : faker.helpers.enumValue(Status),
+            send_at: faker.date.between({from: '20/04/2017', to: '15/05/2023'}),
+            subject : faker.word.adjective(),
+            content: faker.lorem.paragraph({min:1, max:5}),
+        } 
+    }
+}
